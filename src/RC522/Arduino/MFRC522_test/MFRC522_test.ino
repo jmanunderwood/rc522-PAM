2790e4129ad2f20e68476195f70a3bd0a43deb81
jmanunderwood <jmanunderwood@gmail.com>
2021-11-20 15:54:47 -0700

    Update MFRC522_test.ino

--- src/RC522/Arduino/MFRC522_test/MFRC522_test.ino
+++ src/RC522/Arduino/MFRC522_test/MFRC522_test.ino
@@ -6,16 +6,42 @@
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   
 
-void setuo() {
+void setup() {
   Serial.begin(9600);
-  SPI.behin();
-  mfrc522.PCF_Init();
+  SPI.begin();
+  mfrc522.PCD_Init();
 }
 
 void loop() {
   MFRC522::MIFARE_Key key;
-  for(byte i=0li<6;i++) key.keyByte[i]=0xFF;
+  for(byte i=0;i<6;i++) key.keyByte[i]=0xFF;
 
   byte block;
   byte len;
+
+  MFRC522::StatusCode status;
+
+  if(!mfrc522.PICC_IsNewCardPresent()){
+    return;
+  }
+  if(!mfrc522.PICC_ReadCardSerial()){
+    return;
+  }
+  Serial.println(F("Card Detected.."));
+
+  mfrc522.PICC_DumpToSerial(&mfrc522.uid); //Dump raw blocks
+
+  byte buffer[18]
+
+  block=10;
+  len=18;
+
+  status=mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 10, &key, &mfrc522.uid);
+  if(status != MFRC522::STATUS_OK){
+    Serial.print("Authentication Failure: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+
 }
