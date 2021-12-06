5d3040622130da76d345ffea65f2ef62416dbb60
jmanunderwood <jmanunderwood@gmail.com>
2021-12-06 12:41:55 -0700

    Update rc522.cpp

--- src/PAM/rc522.cpp
+++ src/PAM/rc522.cpp
@@ -11,19 +11,19 @@
 
 
 constexpr const char* const SERIAL_PORT = "/dev/ttyACM0";
+const char* module_data_name = "CARD_ID";
 
 PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
 	return PAM_SUCCESS;
 }
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv){
-    
+    /*
     int retval;
     const char* uname;
 
-    std::string user = "";
+    std::string ID = "";
     std::string serial_in = "";
-    std::string pass = "";
 
     LibSerial::SerialPort serial_port;
 
@@ -44,14 +44,34 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
     serial_port.SetParity(LibSerial::Parity::PARITY_NONE);
     serial_port.SetStopBits(LibSerial::StopBits::STOP_BITS_1);
     
-    retval = pam_get_user(pamh, &uname, "Test: ");
+    while(!serial_port.IsDataAvailable()){
+        usleep(1000);
+    }
 
+    size_t timeout = 250;
+    LibSerial::DataBuffer read_buf;
 
+    while(serial_in==""){ //TODO: eek
+        try{
+            serial_port.Read(read_buf,0,timeout);
+        }catch(const LibSerial::ReadTimeout&){
+            for(size_t i=0;i<read_buf.size();i++)
+                serial_in += read_buf.at(i);
+            
+            std::cout<<"."<<std::endl;
+        }
+    }
+    ID = serial_in.substr(0,serial_in.find('\n'));
+    ID.erase(ID.find_first_of(' '), ID.length()-ID.find_first_of(' '));
+
+    uname = const_cast<char*>(ID.c_str());
+    const void** data;
+    retval = pam_get_data(pamh, module_data_name, data);
 
     if (retval!=PAM_SUCCESS){
         return retval;
     }
-    
+    */
     return PAM_SUCCESS;
 }
 
